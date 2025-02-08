d9d4fe5eacdddefe007a7b2ae3f1d98d89338d55
IreneDeng0202 <irenedeng0202@gmail.com>
2025-02-08 17:42:43 +1300

    Update init_database.sql

--- script/init_database.sql
+++ script/init_database.sql
@@ -10,7 +10,7 @@ Script Purpose:
 WARNING:
     Running this script will drop the entire 'DataWarehouse' database if it exists. 
     All data in the database will be permanently deleted. Proceed with caution and ensure you have proper backups.
-/*
+*/
 
 USE master;
 
